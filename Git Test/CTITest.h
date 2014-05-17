f62ee406e8e5ee484eaeb6780b9ff84d79c02b07
Cyrus Tau <monika@monikas-MacBook-Pro.local>
2014-05-17 10:34:34 -0700

    added a test class

---
+++ Git Test/CTITest.h
@@ -0,0 +1,13 @@
+//
+//  CTITest.h
+//  Git Test
+//
+//  Created by Monika Gorkani on 5/17/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTITest : NSObject
+
+@end
---
+++ Git Test/CTITest.m
@@ -0,0 +1,13 @@
+//
+//  CTITest.m
+//  Git Test
+//
+//  Created by Monika Gorkani on 5/17/14.
+//  Copyright (c) 2014 Cyrus. All rights reserved.
+//
+
+#import "CTITest.h"
+
+@implementation CTITest
+
+@end
